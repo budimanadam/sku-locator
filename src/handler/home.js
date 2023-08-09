c200fe3fb2e38204197722023163f60aff46c65d
adam <adam@jubelio.com>
2023-08-09 17:02:53 +0700

    fixed q in get skuLocations

--- src/handler/home.js
+++ src/handler/home.js
@@ -3,16 +3,18 @@ const getHome = async (req, rep) => {
     let param = req.query;
     let filter = '';
     if (param && param.q) {
-        filter = 'where i.item_code like ?';
-        q = param.q;
+        filter = 'where i.item_code like ? or i.item_name like ? or b.bin_code like ? or b.bin_name like ?';
+        q = `%${param.q}%`
     }
+    console.log('filter');
+    console.log(filter);
     const [skuLocations] = await req.db.execute(`
         select i.item_name, i.item_code, i.stock, b.bin_name, b.bin_code
         from bin_item_activity bia 
         join bin b on b.bin_id = bia.bin_id 
         join item i on i.item_id = bia.item_id 
         ${filter}
-    `, [`%${q}%`]);
+    `, [q, q, q, q]);
     return rep.view("/templates/index.ejs", { skuLocations });
 }
 
