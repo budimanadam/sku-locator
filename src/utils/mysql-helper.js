dbf865f97068997af884c27ddc554922556088ea
adam <adam@jubelio.com>
2023-08-16 21:16:29 +0700

    comment out mysql herlpler

--- src/utils/mysql-helper.js
+++ src/utils/mysql-helper.js
@@ -1,28 +1,28 @@
-const config = require('../config');
+// const config = require('../config');
 
-// get the client
-const mysql = require('mysql2/promise');
+// // get the client
+// const mysql = require('mysql2/promise');
 
-// get the promise implementation, we will use bluebird
-const bluebird = require('bluebird');
+// // get the promise implementation, we will use bluebird
+// const bluebird = require('bluebird');
 
 
-function connect() {
-    return mysql.createPool({
-    host:config.HOST, 
-    user: config.USER, 
-    password: config.PASSWORD, 
-    database: config.DATABASE, 
-    port: config.DB_PORT,
-    // Promise: bluebird,
-    waitForConnections: true,
-    connectionLimit: 1000,
-    maxIdle: 1000, // max idle connections, the default value is the same as `connectionLimit`
-    idleTimeout: 6000000, // idle connections timeout, in milliseconds, the default value 60000
-    queueLimit: 0,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 0
-  });
-}
+// function connect() {
+//     return mysql.createPool({
+//     host:config.HOST, 
+//     user: config.USER, 
+//     password: config.PASSWORD, 
+//     database: config.DATABASE, 
+//     port: config.DB_PORT,
+//     // Promise: bluebird,
+//     waitForConnections: true,
+//     connectionLimit: 1000,
+//     maxIdle: 1000, // max idle connections, the default value is the same as `connectionLimit`
+//     idleTimeout: 6000000, // idle connections timeout, in milliseconds, the default value 60000
+//     queueLimit: 0,
+//     enableKeepAlive: true,
+//     keepAliveInitialDelay: 0
+//   });
+// }
 
-module.exports = connect;
+// module.exports = connect;
