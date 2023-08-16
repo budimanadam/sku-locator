56eb06ef087f0c3a6a040c864d4c9d818ed86e1f
adam <adam@jubelio.com>
2023-08-16 11:28:35 +0700

    adding pool

--- src/utils/mysql-helper.js
+++ src/utils/mysql-helper.js
@@ -8,7 +8,21 @@ const bluebird = require('bluebird');
 
 
 function connect() {
-    return mysql.createConnection({host:config.HOST, user: config.USER, password: config.PASSWORD, database: config.DATABASE, port: config.DB_PORT, Promise: bluebird});
+    return mysql.createPool({
+    host:config.HOST, 
+    user: config.USER, 
+    password: config.PASSWORD, 
+    database: config.DATABASE, 
+    port: config.DB_PORT,
+    Promise: bluebird,
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0
+  });
 }
 
 module.exports = connect;
